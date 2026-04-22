43e0e1a61e609cbc4164ab4054bdd14a99405bc5
Eric Frye <eric.frye25@gmail.com>
2026-04-22 14:40:13 -0400

    add Franchise

---
+++ ImitationOnlineOrderingDatabase/Franchise.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Franchise](
+	[FranchiseID] [int] IDENTITY(1,1) NOT NULL,
+	[FranchiseName] [nvarchar](100) NOT NULL,
+	[FranchiseOwnerUserID] [uniqueidentifier] NULL,
+	CONSTRAINT [PK_Franchise] PRIMARY KEY CLUSTERED 
+	(
+		[FranchiseID] ASC
+	)
+)
+GO
--- ImitationOnlineOrderingDatabase/Restaurant.sql
+++ ImitationOnlineOrderingDatabase/Restaurant.sql
@@ -1,10 +1,16 @@
 ﻿CREATE TABLE [dbo].[Restaurant](
 	[RestaurantID] [int] IDENTITY(1,1) NOT NULL,
 	[RestaurantName] [nvarchar](200) NOT NULL,
-	[UserID] [uniqueidentifier] NOT NULL,
+	[FranchiseID] [int] NOT NULL,
+	[RestaurantOwnerUserID] [uniqueidentifier] NULL,
 	CONSTRAINT [PK_Restuarant] PRIMARY KEY CLUSTERED 
 	(
 		[RestaurantID] ASC
 	)
 )
+GO
+
+ALTER TABLE [dbo].[Restaurant] ADD CONSTRAINT [FK_Restaurant_Franchise] FOREIGN KEY([FranchiseID])
+REFERENCES [dbo].[Franchise] ([FranchiseID])
+ON DELETE CASCADE
 GO
